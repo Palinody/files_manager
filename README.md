4ceb9ca59575b3d2da846f0f13abf77620bd74ae
Palinody <lucas.klepic@ipsa.fr>
2021-10-16 20:24:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,186 @@
+- Example directories tree
+```bash
+<root directory>
+├── empty_dir
+├── file_0
+├── file_1
+├── file_2
+├── folder_0
+│   ├── file_3
+│   ├── file_4
+│   ├── file_5
+│   ├── file_6
+│   ├── folder_0
+│   │   └── folder_0
+│   │       ├── file_7
+│   │       ├── file_8
+│   │       ├── folder_0
+│   │       │   ├── file_10
+│   │       │   ├── file_11
+│   │       │   └── file_12
+│   │       ├── folder_1
+│   │       └── folder_2
+│   │           └── file_9
+│   ├── folder_1
+│   │   ├── folder_0
+│   │   │   ├── file_14
+│   │   │   ├── file_15
+│   │   │   ├── file_17
+│   │   │   └── file_18
+│   │   └── folder_1
+│   │       ├── file_19
+│   │       └── file_20
+│   └── folder_2
+│       └── file_13
+├── folder_1
+│   └── front_center
+└── folder2
+    └── file_21
+```
+
+- Define `root` directory
+```bash
+$ python3 files_manager.py --root /path/to/root/directory
+```
+This command will silently construct the parse tree
+```bash
+
+```
+
+- To visualise the tree parser, use the cmd `--verbose` or `-V`
+```bash
+$ python3 files_manager.py --root /path/to/root/directory --verbose
+```
+The resulting tree will parse only the files 
+```bash
+Path:  /path/to/root/directory
+         SIZE (byte)     DATE       TIME     PATH
+         226,948,380     09-03-2021 16:46:13 file_1
+         161,836,709     26-02-2021 09:27:38 file_2
+         361,543,919     14-10-2021 08:07:51 file_0
+```
+
+- To parse the directories recursively use `--recursion` or `-r`
+```bash
+$ python3 files_manager.py --root /path/to/root/directory -V -r
+```
+```bash
+Path:  /path/to/root/directory
+         SIZE (byte)     DATE       TIME     PATH
+         226,948,380     09-03-2021 16:46:13 file_1
+         161,836,709     26-02-2021 09:27:38 file_2
+         361,543,919     14-10-2021 08:07:51 file_0
+
+Path:  /path/to/root/directory/folder2
+         SIZE (byte)     DATE       TIME     PATH
+         674,644,297     05-10-2021 08:39:22 file_21
+
+Path:  /path/to/root/directory/folder_0
+         SIZE (byte)     DATE       TIME     PATH
+         176,797,703     10-03-2021 08:51:21 file_6
+         146,947,693     21-05-2021 10:18:46 file_5
+         175,991,234     10-03-2021 08:49:25 file_3
+         173,711,458     10-03-2021 08:48:54 file_4
+
+Path:  /path/to/root/directory/folder_0/folder_1/folder_1
+         SIZE (byte)     DATE       TIME     PATH
+         129,131,325     21-05-2021 13:51:23 file_20
+         146,947,693     21-05-2021 13:44:18 file_19
+
+Path:  /path/to/root/directory/folder_0/folder_1/folder_0
+         SIZE (byte)     DATE       TIME     PATH
+         1,170,405,832   12-03-2021 14:01:16 file_14
+         1,644,261,352   10-05-2021 13:03:26 file_15
+         2,155,620,073   12-03-2021 14:03:28 file_17
+         1,288,557,762   12-03-2021 12:31:56 file_18
+
+Path:  /path/to/root/directory/folder_0/folder_0/folder_0
+         SIZE (byte)     DATE       TIME     PATH
+         2,697           21-05-2021 13:44:35 file_7
+         58,223,158      21-05-2021 13:44:35 file_8
+
+Path:  /path/to/root/directory/folder_0/folder_0/folder_0/folder_0
+         SIZE (byte)     DATE       TIME     PATH
+         1,236,016,848   21-05-2021 13:50:20 file_10
+         119,965,643     21-05-2021 13:50:50 file_12
+         1,056,284,105   21-05-2021 13:55:51 file_11
+
+Path:  /path/to/root/directory/folder_0/folder_0/folder_0/folder_2
+         SIZE (byte)     DATE       TIME     PATH
+         121,736,117     21-05-2021 13:56:22 file_9
+
+Path:  /path/to/root/directory/folder_0/folder_2
+         SIZE (byte)     DATE       TIME     PATH
+         119,965,643     21-05-2021 10:46:29 file_13
+```
+
+- To sort the visualisation of the tree by date or by file size: `--sort size`, `--sort date`, `--sort rSize`, `--sort rDate`. The 2 first options sort in increasing order and the 2 last in reversed/decreasing order.
+```bash
+$ python3 files_manager.py --root /path/to/root/directory -V -r --sort date
+```
+The --sort command has no influence on the tree and only modifies how it is __presented in the console__.
+```bash
+SIZE (byte)     DATE       TIME     PATH
+161,836,709     26-02-2021 09:27:38 /path/to/root/directory/file_2
+226,948,380     09-03-2021 16:46:13 /path/to/root/directory/file_1
+173,711,458     10-03-2021 08:48:54 /path/to/root/directory/folder_0/file_4
+175,991,234     10-03-2021 08:49:25 /path/to/root/directory/folder_0/file_3
+176,797,703     10-03-2021 08:51:21 /path/to/root/directory/folder_0/file_6
+1,288,557,762   12-03-2021 12:31:56 /path/to/root/directory/folder_0/folder_1/folder_0/file_18
+1,170,405,832   12-03-2021 14:01:16 /path/to/root/directory/folder_0/folder_1/folder_0/file_14
+2,155,620,073   12-03-2021 14:03:28 /path/to/root/directory/folder_0/folder_1/folder_0/file_17
+1,644,261,352   10-05-2021 13:03:26 /path/to/root/directory/folder_0/folder_1/folder_0/file_15
+146,947,693     21-05-2021 10:18:46 /path/to/root/directory/folder_0/file_5
+119,965,643     21-05-2021 10:46:29 /path/to/root/directory/folder_0/folder_2/file_13
+146,947,693     21-05-2021 13:44:18 /path/to/root/directory/folder_0/folder_1/folder_1/file_19
+58,223,158      21-05-2021 13:44:35 /path/to/root/directory/folder_0/folder_0/folder_0/file_8
+2,697           21-05-2021 13:44:35 /path/to/root/directory/folder_0/folder_0/folder_0/file_7
+1,236,016,848   21-05-2021 13:50:20 /path/to/root/directory/folder_0/folder_0/folder_0/folder_0/file_10
+119,965,643     21-05-2021 13:50:50 /path/to/root/directory/folder_0/folder_0/folder_0/folder_0/file_12
+129,131,325     21-05-2021 13:51:23 /path/to/root/directory/folder_0/folder_1/folder_1/file_20
+1,056,284,105   21-05-2021 13:55:51 /path/to/root/directory/folder_0/folder_0/folder_0/folder_0/file_11
+121,736,117     21-05-2021 13:56:22 /path/to/root/directory/folder_0/folder_0/folder_0/folder_2/file_9
+674,644,297     05-10-2021 08:39:22 /path/to/root/directory/folder2/file_21
+361,543,919     14-10-2021 08:07:51 /path/to/root/directory/file_0
+```
+
+- The tree can be filtered using the `--filter` option by date or by file size with rules. 
+There are currently 2 rules:
+
+File size filtering: `<comparator> <integer> <file size unit>`
+
+Timestamp filtering: `<comparator> <timestamp>`
+
+`comparator`: "lt" or "gt" meaning "less than" and "greater than" respectively.
+
+`file size unit`: "G" or "M" or "K" meaning gigabyte, megabyte and kilobyte respectively
+
+`timestamp`: "DD_MM_YYYY" or "DD_MM_YYYY:hh_mm_ss"
+
+Example
+```bash
+python3 files_manager.py --root /path/to/root/directory -V -r --sort date --filter gt 21_05_2021:13_44_18
+```
+The above tree becomes:
+```bash
+SIZE (byte)     DATE       TIME     PATH
+146,947,693     21-05-2021 13:44:18 /path/to/root/directory/folder_0/folder_1/folder_1/file_19
+58,223,158      21-05-2021 13:44:35 /path/to/root/directory/folder_0/folder_0/folder_0/file_8
+2,697           21-05-2021 13:44:35 /path/to/root/directory/folder_0/folder_0/folder_0/file_7
+1,236,016,848   21-05-2021 13:50:20 /path/to/root/directory/folder_0/folder_0/folder_0/folder_0/file_10
+119,965,643     21-05-2021 13:50:50 /path/to/root/directory/folder_0/folder_0/folder_0/folder_0/file_12
+129,131,325     21-05-2021 13:51:23 /path/to/root/directory/folder_0/folder_1/folder_1/file_20
+1,056,284,105   21-05-2021 13:55:51 /path/to/root/directory/folder_0/folder_0/folder_0/folder_0/file_11
+121,736,117     21-05-2021 13:56:22 /path/to/root/directory/folder_0/folder_0/folder_0/folder_2/file_9
+674,644,297     05-10-2021 08:39:22 /path/to/root/directory/folder2/file_21
+361,543,919     14-10-2021 08:07:51 /path/to/root/directory/file_0
+```
+
+- Finally, to remove all the files that are shown after applying all the filters, use `--remove`
+```bash
+python3 files_manager.py --root /path/to/root/directory -V -r --sort date --filter gt 21_05_2021:13_44_18 --remove
+```
+Of course, you can remove the files directly using the rule without outputing in the console. Though, it's safer to visualise the tree first.
+```bash
+python3 files_manager.py --root /path/to/root/directory -r --filter gt 21_05_2021:13_44_18 --remove
+```
---
+++ files_manager.py
@@ -0,0 +1,265 @@
+"""
+Files manager
+"""
+import datetime, time
+import argparse
+import sys, os
+
+from typing import Tuple, List, Dict, Any
+
+def generate_file_info(file_paths:List[str], dirpath:str="") -> Tuple[int, float, str]:
+    """
+    iterates over a list of file names and yields a  3-tuple containing the file size, its creation date
+    """
+    for fn in file_paths:
+        # get full path of file
+        file_path:str = os.path.join(dirpath, fn)
+        # get size in kilobytes
+        file_size:int = os.path.getsize(file_path)
+        file_creation_date:float = os.path.getmtime(file_path)
+        yield file_size, file_creation_date, fn
+
+def filter_size_to_logic(value:int, filter:Tuple[str, str, str]) -> bool:
+    filter_value:int = int(filter[1])
+    if filter[0] == "lt":
+        if filter[2] == 'G':
+            return (value < filter_value * 1e9)
+        elif filter[2] == 'M':
+            return (value < filter_value * 1e6)
+        elif filter[2] == 'K':
+            return (value < filter_value * 1e3)
+    elif filter[0] == "gt":
+        if filter[2] == 'G':
+            return (value > filter_value * 1e9)
+        elif filter[2] == 'M':
+            return (value > filter_value * 1e6)
+        elif filter[2] == 'K':
+            return (value > filter_value * 1e3)
+    else:
+        raise ValueError
+
+def filter_timestamp_to_logic(timestamp:float, filter:Tuple[str, str]) -> bool:
+    filter_timestamp:float = check_transform_timestamp(filter[1])
+    if filter[0] == "lt":
+        return (timestamp < filter_timestamp)
+    elif filter[0] == "gt":
+        return (timestamp > filter_timestamp)
+    else:
+        raise ValueError
+
+def make_tree(root:str, filter:List[str], recursion:bool=True) -> Dict[str, List[Tuple[int, float, str]]]:
+    """
+    Makes a dict(keys : values)
+    keys: the directory full path
+    values: each file of the directory
+    If a filter is provided, we keep only data that is relevant to the filter's rule
+    If recursion is false we only add files from the root directory
+    """
+    tree:Dict[str, List[Tuple[int, float, str]]] = {}
+    for dirpath, dirnames, filenames in os.walk(root):
+        # If a filter is provided
+        if filter:
+            # check if it's a file size comparator filter
+            if is_size_filer(filter):
+                tree[dirpath] = [(size, timestamp, fn) for size, timestamp, fn in generate_file_info(filenames, dirpath=dirpath) if filter_size_to_logic(size, filter)]
+            # or a timestamp comparator filter
+            elif is_timestamp_filter(filter):
+                tree[dirpath] = [(size, timestamp, fn) for size, timestamp, fn in generate_file_info(filenames, dirpath=dirpath) if filter_timestamp_to_logic(timestamp, filter)]
+        else:
+            tree[dirpath] = [*generate_file_info(filenames, dirpath=dirpath)]
+        if not recursion:
+            return tree
+    return tree
+
+def get_tree_values_number(tree:Dict[Any, Any]) -> int:
+    return sum(len(v) for v in tree.itervalues())
+
+def make_list_from_dict_tree(tree:Dict[str, List[Tuple[Any, Any, str]]]) -> List[Tuple[Any, Any, str]]:
+    """
+    Converts from dict encoding tree to list encoding tree.
+    The loop concatenates each key (dir. path) to their corresponding values (get file_name from file_info) and merge the resulting list.
+    """
+    files_list:List[Tuple[Any, Any, str]] = []
+    for keys, values in tree.items():
+        # skip empty folders
+        if values:
+            files_list += [(size, date, os.path.join(keys, fn)) for size, date, fn in values]
+    return files_list
+
+def format_file_info(file_info:Tuple[int, float, str]) -> Tuple[str, str, str]:
+    """
+    pretty formatting for file_info
+    currently:
+    - File size: puts commas for big numbers 1000000 -> 1,000,000
+    - File save date: UNIX time to prettier "dd-mm-YYYY HH:MM:SS"
+    - File path: full
+    """
+    return ("{:,d}".format(file_info[0]), 
+            datetime.datetime.utcfromtimestamp(file_info[1]).strftime('%d-%m-%Y %H:%M:%S'),
+            file_info[2])
+
+def print_tree(tree:Dict[str, List[Tuple[int, float, str]]], pretty_formatter:str, header:str="") -> None:
+    """
+    Simply iterates over the keys (folder_path) and prints the values (file_info) line by line.
+    The keys are printed before each file_info chunk and we add a tab before each entry of the latter.
+    """
+    for key, values in tree.items():
+        #  ignore empty lists
+        if not values: 
+            continue
+        print("Path: ", key)
+        print("\t", header)
+        for file_info in values:
+            print("\t", pretty_formatter%(format_file_info(file_info)))
+        print()
+
+def print_tree_with_sorting_args(tree:Dict[str, List[Tuple[int, float, str]]], sorting_rule:str=None) -> None:
+    """
+    rules: 
+    - size: file size in bytes
+    - date: date in unix time
+    Each rule has its own reversed rule version.
+
+    The lambdas specify w.r.t. which file_info entry we should sort.
+    """
+    header:str = "%s %8s %10s %8s"%("SIZE (byte)", "DATE", "TIME", "PATH")
+    pretty_formatter:str = '%-15s %s %s'
+    if not sorting_rule:
+        print_tree(tree, pretty_formatter, header)
+    else:
+        print(header)
+        if sorting_rule == "size":
+            for file_info in sorted(make_list_from_dict_tree(tree), key=lambda x: x[0]):
+                # print("{:15,d}".format(format_file_info(file_info)[0]) + " %s %s"%format_file_info(file_info)[1:])
+                print(pretty_formatter%(format_file_info(file_info)))
+        elif sorting_rule == "rSize":
+            for file_info in sorted(make_list_from_dict_tree(tree), key=lambda x: x[0], reverse=True):
+                # print("{:15,d}".format(format_file_info(file_info)[0]) + " %s %s"%format_file_info(file_info)[1:])
+                print(pretty_formatter%(format_file_info(file_info)))
+        elif sorting_rule == "date":
+            for file_info in sorted(make_list_from_dict_tree(tree), key=lambda x: x[1]):
+                print(pretty_formatter%(format_file_info(file_info)))
+        elif sorting_rule == "rDate":
+            for file_info in sorted(make_list_from_dict_tree(tree), key=lambda x: x[1], reverse=True):
+                print(pretty_formatter%(format_file_info(file_info)))
+
+def check_transform_timestamp(timestamp:str) -> float:
+    """
+    Checks and transforms timestamp to unix time format.
+    valid formats:
+    - "DD_MM_YYYY"
+    - "DD_MM_YYYY:HH_MM_SS"\n
+    If valid, transforms to unix.
+    """
+    unix_timestamp:float=None
+    try:
+        try:
+            date, time_ = timestamp.split(':')
+            DD, MM, YYYY = tuple(int(x) for x in date.split("_"))
+            hh, mm, ss = tuple(int(x) for x in time_.split("_"))
+            date_time = datetime.datetime(YYYY, MM, DD, hh, mm, ss)
+            unix_timestamp = time.mktime(date_time.timetuple())
+        except:
+            DD, MM, YYYY = tuple(int(x) for x in timestamp.split("_"))
+            date_time = datetime.datetime(YYYY, MM, DD)
+            unix_timestamp = time.mktime(date_time.timetuple())
+    except:
+        print("Incorrect timestamp format, must be DD_MM_YYYY or DD_MM_YYYY:hh_mm_ss.\nWas: %s"%(timestamp))
+        raise ValueError
+    # adjust to local time
+    return unix_timestamp + (time.mktime(datetime.datetime.now().timetuple()) - time.mktime(datetime.datetime.utcnow().timetuple()))
+
+def is_size_filer(filter:List[str]) -> bool:
+    """
+    Checks the size file size.
+    lt, gt = less, greater than.
+    """
+    if len(filter) == 3 \
+        and (filter[0] == "lt" or filter[0] == "gt") \
+        and (filter[1].isdigit()) \
+        and (filter[2] == 'G' or filter[2] == 'M' or filter[2] == 'K'):
+        return True
+    return False
+
+def is_timestamp_filter(filter:List[str]) -> bool:
+    """
+    Checks timestamp filter.
+    lt, gt = less, greater than.
+    """
+    if len(filter) == 2 \
+        and (filter[0] == "lt" or filter[0] == "gt") \
+        and check_transform_timestamp(filter[1]):
+        return True
+    return False
+
+def filter_check_return(filter:List[str]) -> List[str]:
+    """
+    Checks and returns the same filter if success.
+    """
+    assert is_size_filer(filter) or is_timestamp_filter(filter), \
+        """Assertion failed, --filter option must be one of the following:
+            <lt | gt> <integer> <G | M | T>
+            <lt | gt> <DD_MM_YYYY | DD_MM_YYYY:HH_MM_SS>"""
+    return filter
+
+"""
+- os.remove() removes a file.
+- os.rmdir() removes an empty directory.
+- shutil.rmtree() deletes a directory and all its contents.
+python3.4+
+- pathlib.Path.unlink() removes a file or symbolic link.
+- pathlib.Path.rmdir() removes an empty directory.
+"""
+def remove_files(tree:Dict[str, List[Tuple[int, float, str]]]) -> None:
+    for dirpath, values in tree.items():
+        for file_info in values:
+            os.remove(os.path.join(dirpath, file_info[2])) 
+
+def main():
+    # python3 ros_bag_purge.py --root /home/$USER/test_workspace/test_directory -r --verbose --filter lt 09_03_2021:16_46_00 --sort date --remove
+    # Don't take script name
+    args = sys.argv[1:]
+    # Parser instance
+    parser = argparse.ArgumentParser("Purge files from a root directory.")
+    
+    parser.add_argument("--root", nargs="?", help="Root directory from which we enable purge. This option is required.", required=True)
+    parser.add_argument("--recursion", "-r", action="store_true", help="Recursion option.")
+    # parser.add_argument("--depth", nargs="?", type=int, help="Recursion depth. If recursion is specified and depth is not, all the nodes will be traversed.")
+    parser.add_argument("--filter", "-f", nargs="+", help="""
+        Catches files with specified attributes (size or date).
+        """)
+    
+    parser.add_argument("--remove", action="store_true", help="Will remove all the files based on the tree that has been constructed.")
+    
+    parser.add_argument("--verbose", "-V", action="store_true", help="Shows which files have been selected from the chosen root dir.")
+    parser.add_argument("--sort", choices=["size", "rSize", "date", "rDate"], 
+        help="""
+        Shows which files have been selected from the chosen root dir.
+        Choices are sorting options. "r" prefix stands for reverse.
+
+        "size": sort by increasing file size
+        "rSize": sort by decreasing file size
+        "date": sort by increasing date
+        "rDate": sort by decreasing date
+        """)
+    # Move the files in tree to dest
+    # TODO: --move <dest>
+    # Prune all the empty directories
+    # TODO: --prune
+    
+    args = parser.parse_args(args)
+    # Constructs a tree based on the arguments: make_tree(where, parsing filter, recursion?)
+    tree = make_tree(
+        args.root, 
+        filter_check_return(args.filter) if args.filter else None, 
+        args.recursion)
+    # Prints it to console if verbose is True with the specified sorting rules
+    if args.verbose:
+        print_tree_with_sorting_args(tree, args.sort)
+    
+    if args.remove:
+        remove_files(tree)
+
+    print(args)
+if __name__ == "__main__":
+    main()
